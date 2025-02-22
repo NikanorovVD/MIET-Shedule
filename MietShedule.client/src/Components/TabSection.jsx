5f8355f5c7ec6bef2bf5f70d0bb7c32132ced253
NikanorovVD <nikanorovvsevolod1@gmail.com>
2025-02-22 10:58:23 +0300

    TabSection margin

--- MietShedule.client/src/Components/TabSection.jsx
+++ MietShedule.client/src/Components/TabSection.jsx
@@ -1,8 +1,9 @@
 import Button from "./Button/Button"
+import "./TabSection.css"
 
 export default function TabSection({ activeTab, OnChange }) {
     return (
-        <section>
+        <section className="tab-section">
             <Button active={activeTab == 'shedule'} onClick={() => OnChange('shedule')}>Расписание</Button>
             <Button active={activeTab == 'teacher'} onClick={() => OnChange('teacher')}>Поиск преподавателя</Button>
             <Button active={activeTab == 'export'} onClick={() => OnChange('export')}>Экспорт расписания</Button>
