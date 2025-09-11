181e6f681c1e423c44c8b3b1c76495a6341b37b5
NikanorovVD <nikanorovvsevolod1@gmail.com>
2025-09-11 12:00:38 +0300

    Add docker

--- Frontend/MietShedule.client/vite.config.js
+++ Frontend/MietShedule.client/vite.config.js
@@ -1,44 +1,12 @@
 import { fileURLToPath, URL } from 'node:url';
-
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import plugin from '@vitejs/plugin-react';
-import fs from 'fs';
-import path from 'path';
-import child_process from 'child_process';
+import mkcert from 'vite-plugin-mkcert'
 
-const baseFolder =
-    process.env.APPDATA !== undefined && process.env.APPDATA !== ''
-        ? `${process.env.APPDATA}/ASP.NET/https`
-        : `${process.env.HOME}/.aspnet/https`;
-
-const certificateArg = process.argv.map(arg => arg.match(/--name=(?<value>.+)/i)).filter(Boolean)[0];
-const certificateName = certificateArg ? certificateArg.groups.value : "MietShedule.client";
-
-if (!certificateName) {
-    console.error('Invalid certificate name. Run this script in the context of an npm/yarn script or pass --name=<<app>> explicitly.')
-    process.exit(-1);
-}
-
-const certFilePath = path.join(baseFolder, `${certificateName}.pem`);
-const keyFilePath = path.join(baseFolder, `${certificateName}.key`);
-
-if (!fs.existsSync(certFilePath) || !fs.existsSync(keyFilePath)) {
-    if (0 !== child_process.spawnSync('dotnet', [
-        'dev-certs',
-        'https',
-        '--export-path',
-        certFilePath,
-        '--format',
-        'Pem',
-        '--no-password',
-    ], { stdio: 'inherit', }).status) {
-        throw new Error("Could not create certificate.");
-    }
-}
-
-// https://vitejs.dev/config/
+process.env = {...process.env, ...loadEnv('', process.cwd())};
+console.log(process.env.VITE_APP_URL)
 export default defineConfig({
-    plugins: [plugin()],
+    plugins: [plugin(), mkcert()],
     resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url))
@@ -47,28 +15,24 @@ export default defineConfig({
     server: {
         proxy: {
             '^/Groups': {
-                target: 'https://localhost:7056/',
+                target: process.env.VITE_APP_URL,
                 secure: false
             },
-            '^/Shedule':{
-                target: 'https://localhost:7056/',
+            '^/Shedule': {
+                target: process.env.VITE_APP_URL,
                 secure: false
             },
-            '^/Teachers':{
-                target: 'https://localhost:7056/',
+            '^/Teachers': {
+                target: process.env.VITE_APP_URL,
                 secure: false
             },
-            '^/Export':{
-                target: 'https://localhost:7056/',
+            '^/Export': {
+                target: process.env.VITE_APP_URL,
                 secure: false
             },
-            
+
         },
-        port: 5173,
-        https: {
-            key: fs.readFileSync(keyFilePath),
-            cert: fs.readFileSync(certFilePath),
-        },
-        host: '0.0.0.0'
+        host: '0.0.0.0',
+        port: 5173
     }
 })
